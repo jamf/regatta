46d1a5314670159dc84ad48b836d50a72257c0a3
jakub.coufal <jakub.coufal@wandera.com>
2020-10-06 13:01:05 +0200

    Fix variable shadowing

--- regattaserver/maintenance_test.go
+++ regattaserver/maintenance_test.go
@@ -13,7 +13,7 @@ import (
 var ms MaintenanceServer
 
 func TestRegatta_Reset(t *testing.T) {
-	require := require.New(t)
+	r := require.New(t)
 
 	t.Log("Put kv")
 	_, err := kv.Put(context.Background(), &proto.PutRequest{
@@ -21,37 +21,36 @@ func TestRegatta_Reset(t *testing.T) {
 		Key:   key1Name,
 		Value: table1Value1,
 	})
-	require.NoError(err, "Failed to put kv")
+	r.NoError(err, "Failed to put kv")
 
 	t.Log("Check kv exists")
 	_, err = kv.Range(context.Background(), &proto.RangeRequest{
 		Table: table1Name,
 		Key:   key1Name,
 	})
-	require.NoError(err, "Failed to get value")
+	r.NoError(err, "Failed to get value")
 
 	t.Log("Reset")
 	_, err = ms.Reset(context.Background(), &proto.ResetRequest{})
-	require.NoError(err, "Failed to reset")
+	r.NoError(err, "Failed to reset")
 
 	t.Log("Check kv doesn't exist")
 	_, err = kv.Range(context.Background(), &proto.RangeRequest{
 		Table: table1Name,
 		Key:   key1Name,
 	})
-	require.EqualError(err, status.Errorf(codes.NotFound, "key not found").Error())
+	r.EqualError(err, status.Errorf(codes.NotFound, "key not found").Error())
 }
 
 func TestRegatta_Hash(t *testing.T) {
-	require := require.New(t)
-
+	r := require.New(t)
 	t.Log("Reset")
 	_, err := ms.Reset(context.Background(), &proto.ResetRequest{})
-	require.NoError(err)
+	r.NoError(err)
 
 	t.Log("Get Hash")
 	hash, err := ms.Hash(context.Background(), &proto.HashRequest{})
-	require.NoError(err)
+	r.NoError(err)
 
-	require.Equal(uint64(5001005189967390176), hash.Hash)
+	r.Equal(uint64(5001005189967390176), hash.Hash)
 }
