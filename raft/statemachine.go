4d1021a1d6a8489b474cedf39600d79bdb34492e
jakub.coufal <jakub.coufal@wandera.com>
2020-10-06 09:59:33 +0200

    Dragonboat bootstrap - implement in-memory statemachine

--- raft/statemachine.go
+++ raft/statemachine.go
@@ -1,71 +1,87 @@
 package raft
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"time"
 
 	sm "github.com/lni/dragonboat/v3/statemachine"
+	"github.com/wandera/regatta/proto"
+	"github.com/wandera/regatta/storage"
+	pb "google.golang.org/protobuf/proto"
 )
 
 func NewStateMachine(clusterID uint64, nodeID uint64) sm.IStateMachine {
-	return &PersistentStateMachine{}
+	return &PersistentStateMachine{
+		storage: make(map[string][]byte),
+	}
 }
 
 // PersistentStateMachine is a IStateMachine struct used for testing purpose.
 type PersistentStateMachine struct {
-	MillisecondToSleep uint64
-	NoAlloc            bool
+	storage map[string][]byte
 }
 
 // Lookup locally looks up the data.
 func (n *PersistentStateMachine) Lookup(key interface{}) (interface{}, error) {
-	return make([]byte, 1), nil
-}
-
-// NALookup locally looks up the data.
-func (n *PersistentStateMachine) NALookup(key []byte) ([]byte, error) {
-	return key, nil
+	if value, ok := n.storage[string(key.([]byte))]; ok {
+		return value, nil
+	}
+	return nil, storage.ErrNotFound
 }
 
 // Update updates the object.
 func (n *PersistentStateMachine) Update(data []byte) (sm.Result, error) {
-	if n.MillisecondToSleep > 0 {
-		time.Sleep(time.Duration(n.MillisecondToSleep) * time.Millisecond)
+	cmd := proto.Command{}
+	err := pb.Unmarshal(data, &cmd)
+	if err != nil {
+		return sm.Result{}, err
 	}
-	if n.NoAlloc {
-		return sm.Result{Value: uint64(len(data))}, nil
+
+	key := string(append(cmd.Table, cmd.Kv.Key...))
+	switch cmd.Type {
+	case proto.Command_PUT:
+		n.storage[key] = cmd.Kv.Value
+		return sm.Result{Value: 1}, nil
+	case proto.Command_DELETE:
+		if _, ok := n.storage[key]; ok {
+			delete(n.storage, key)
+			return sm.Result{Value: 1}, nil
+		}
 	}
-	v := make([]byte, len(data))
-	copy(v, data)
-	return sm.Result{Value: uint64(len(data)), Data: v}, nil
+
+	return sm.Result{Value: 0}, nil
 }
 
 // SaveSnapshot saves the state of the object to the provided io.Writer object.
 func (n *PersistentStateMachine) SaveSnapshot(w io.Writer, fileCollection sm.ISnapshotFileCollection, done <-chan struct{}) error {
-	data, err := json.Marshal(n)
+	data, err := json.Marshal(n.storage)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
 	_, err = w.Write(data)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
 // RecoverFromSnapshot recovers the object from the snapshot specified by the
 // io.Reader object.
 func (n *PersistentStateMachine) RecoverFromSnapshot(r io.Reader, files []sm.SnapshotFile, done <-chan struct{}) error {
-	var sn PersistentStateMachine
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
-	err = json.Unmarshal(data, &sn)
+
+	err = json.Unmarshal(data, &n.storage)
 	if err != nil {
-		panic("failed to unmarshal snapshot")
+		return err
 	}
 
 	return nil
@@ -76,5 +92,10 @@ func (n *PersistentStateMachine) Close() error { return nil }
 
 // GetHash returns a uint64 value representing the current state of the object.
 func (n *PersistentStateMachine) GetHash() (uint64, error) {
-	return 0, nil
+	var b bytes.Buffer
+	err := gob.NewEncoder(&b).Encode(n.storage)
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint64(b.Bytes()), nil
 }
