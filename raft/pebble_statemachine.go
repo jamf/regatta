51f53488a3a30e374d115c2e4e5208adcdc3e4c8
Josef Janda <josef.janda@wandera.com>
2020-10-09 13:01:55 +0200

    WND-30026 optimizations, batch ops, pebbleWalDir

--- raft/pebble_statemachine.go
+++ raft/pebble_statemachine.go
@@ -1,10 +1,10 @@
 package raft
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"io"
-	"reflect"
 
 	"go.uber.org/zap"
 
@@ -23,99 +23,118 @@ const (
 	raftLogIndex byte = iota
 )
 
-func NewPebbleStateMachine(_ uint64, _ uint64) sm.IOnDiskStateMachine {
+func NewPebbleStateMachine(clusterID uint64, nodeID uint64, stateMachineDir string, walDirname string) sm.IOnDiskStateMachine {
 	return &KVPebbleStateMachine{
-		pebble: nil, // to be init in the Open
-		dirname: PersistentDirname,
+		pebble:     nil,
+		clusterID:  clusterID,
+		nodeID:     nodeID,
+		dirname:    stateMachineDir,
+		walDirname: walDirname,
 	}
 }
 
-// TODO Q: How do we test this? Should the methods like Lookup be tested in unit tests? If so, we need to provide in memory FS for pebble.
 // KVStateMachine is a IStateMachine struct used for testing purpose.
 type KVPebbleStateMachine struct {
-	pebble  *pebble.DB
-	dirname string
+	pebble     *pebble.DB
+	clusterID  uint64
+	nodeID     uint64
+	dirname    string
+	walDirname string
 }
 
 func (p *KVPebbleStateMachine) Open(_ <-chan struct{}) (uint64, error) {
 	var err error
+	var walDirname string
 
-	zap.S().Debugf("opening pebble state machine with dirname: %s", p.dirname)
-	p.pebble, err = pebble.Open(p.dirname, &pebble.Options{})
+	dirname := fmt.Sprintf("%s-%d-%d", p.dirname, p.clusterID, p.nodeID)
+	if p.walDirname != "" {
+		walDirname = fmt.Sprintf("%s-%d-%d", p.walDirname, p.clusterID, p.nodeID)
+	}
+	zap.S().Infof("opening pebble state machine with dirname: '%s', walDirName: '%s'", dirname, walDirname)
+
+	p.pebble, err = pebble.Open(dirname, &pebble.Options{
+		WALDir: walDirname,
+	})
 	if err != nil {
-		zap.S().Panic(err) // TODO this is fatal, right? Cannot continue.
+		zap.S().Panic(err)
 	}
 
 	indexVal, closer, err := p.pebble.Get([]byte{kindSystem, raftLogIndex})
-
-	defer func() {
-		if closer != nil {
-			if err := closer.Close(); err != nil {
-				zap.S().Panic("failed to close") // TODO this is fatal, right? Cannot continue.
-			}
-		}
-	}()
-
 	if err != nil {
 		return 0, nil
 	}
 
+	defer func() {
+		if err := closer.Close(); err != nil {
+			zap.S().Warn(err)
+		}
+	}()
+
+	// TODO example: consider adding to field d.lastApplied
 	return binary.LittleEndian.Uint64(indexVal), nil
 }
 
-// TODO flush/sync needed?
 // Update updates the object.
 func (p *KVPebbleStateMachine) Update(updates []sm.Entry) ([]sm.Entry, error) {
-	// TODO inefficient implementation, rework to batch update on the pebble side
-	// this will break if the change is applied only partially - there will be inconsistence between pebble and the raft log
 	cmd := proto.Command{}
+	buf := bytes.NewBuffer(make([]byte, 0))
+	batch := p.pebble.NewBatch()
 	for _, update := range updates {
 		cmd.Reset()
 		err := pb.Unmarshal(update.Cmd, &cmd)
 		if err != nil {
 			update.Result = sm.Result{Value: 0}
-			// TODO not sure about the error handling here in case the incomplete update is done
 			return updates, err
 		}
 
-		// pre-allocate the userDataKey slice for the better performance
-		userDataKey := make([]byte, int(reflect.TypeOf(kindUser).Size())+len(cmd.Table)+len(cmd.Kv.Key)) // TODO Isn't using reflect unnecessarily inefficient?
-		userDataKey[0] = kindUser
-		copy(userDataKey[reflect.TypeOf(kindUser).Size():], cmd.Table)
-		copy(userDataKey[int(reflect.TypeOf(kindUser).Size())+len(cmd.Table):], cmd.Kv.Key)
+		buf.Reset()
+		buf.WriteByte(kindUser)
+		buf.Write(cmd.Table)
+		buf.Write(cmd.Kv.Key)
 
-		raftIndexKey := []byte{kindSystem, raftLogIndex}
-		raftIndexVal := make([]byte, reflect.TypeOf(uint64(0)).Size()) // TODO I've read this might be unsafe though
+		raftIndexVal := make([]byte, 8)
+
 		switch cmd.Type {
 		case proto.Command_PUT:
-			if err := p.pebble.Set(userDataKey, cmd.Kv.Value, nil); err != nil {
+			if err := batch.Set(buf.Bytes(), cmd.Kv.Value, nil); err != nil {
 				update.Result = sm.Result{Value: 0}
-				// TODO not sure about the error handling here in case the incomplete update is done
 				return updates, err
 			}
 
+			buf.Reset()
+			buf.WriteByte(kindSystem)
+			buf.WriteByte(raftLogIndex)
 			binary.LittleEndian.PutUint64(raftIndexVal, update.Index)
-			if err := p.pebble.Set(raftIndexKey, raftIndexVal, nil); err != nil {
+			if err := batch.Set(buf.Bytes(), raftIndexVal, nil); err != nil {
 				update.Result = sm.Result{Value: 0}
-				// TODO not sure about the error handling here in case the incomplete update is done
 				return updates, err
 			}
+
 			update.Result = sm.Result{Value: 1}
 		case proto.Command_DELETE:
-			if err := p.pebble.Delete(userDataKey, nil); err != nil {
+			batch := p.pebble.NewBatch()
+
+			if err := batch.Delete(buf.Bytes(), nil); err != nil {
 				update.Result = sm.Result{Value: 0}
-				// TODO not sure about the error handling here in case the incomplete update is done
 				return updates, err
 			}
 
+			buf.Reset()
+			buf.WriteByte(kindSystem)
+			buf.WriteByte(raftLogIndex)
 			binary.LittleEndian.PutUint64(raftIndexVal, update.Index)
-			if err := p.pebble.Set(raftIndexKey, raftIndexVal, nil); err != nil {
+			if err := batch.Set(buf.Bytes(), raftIndexVal, nil); err != nil {
 				update.Result = sm.Result{Value: 0}
-				// TODO not sure about the error handling here in case the incomplete update is done
 				return updates, err
 			}
+
 			update.Result = sm.Result{Value: 1}
 		}
+		buf.Reset()
+	}
+
+	if err := batch.Commit(nil); err != nil {
+		return updates, err
 	}
 
 	return updates, nil
@@ -123,36 +142,30 @@ func (p *KVPebbleStateMachine) Update(updates []sm.Entry) ([]sm.Entry, error) {
 
 // Lookup locally looks up the data.
 func (p *KVPebbleStateMachine) Lookup(key interface{}) (interface{}, error) {
-	queryKey := make([]byte, len(key.([]byte))+1)
-	queryKey[0] = kindUser
-	copy(queryKey[1:], key.([]byte))
-	value, closer, err := p.pebble.Get(queryKey)
-
-	defer func() {
-		if closer != nil {
-			if err := closer.Close(); err != nil {
-				zap.S().Panic("failed to close") // TODO this is fatal, right? Cannot continue.
-			}
-		}
-	}()
-
+	buf := bytes.NewBuffer(make([]byte, 0))
+	buf.WriteByte(kindUser)
+	buf.Write(key.([]byte))
+	value, closer, err := p.pebble.Get(buf.Bytes())
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO this is not efficient!
-	// closer.Close() must be however called in order to prevent a memory leak
-	// but after calling closer.Close() the value becomes invalid
-	// consider passing closer to the return value as well and close it after the value is returned
-	// to the client
-	return append([]byte(nil), value...), nil
+	defer func() {
+		if err := closer.Close(); err != nil {
+			zap.S().Error(err)
+		}
+	}()
+
+	buf.Reset()
+	buf.Write(value)
+	return buf.Bytes(), nil
 }
 
 // Sync synchronizes all in-core state of the state machine to permanent
 // storage so the state machine can continue from its latest state after
 // reboot.
 func (p *KVPebbleStateMachine) Sync() error {
-	return nil // TODO not implemented, it should not be required in the initial non-batch update style
+	return p.pebble.Flush()
 }
 
 // PrepareSnapshot prepares the snapshot to be concurrently captured and
