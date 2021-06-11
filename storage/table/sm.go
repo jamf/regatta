774efa828d7697bef56d976831cd6fa845b6d792
jakub.coufal <jakub.coufal@wandera.com>
2021-06-11 11:53:51 +0200

    WND-37070 - Per table state machine

---
+++ storage/table/sm.go
@@ -0,0 +1,468 @@
+package table
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/oxtoacart/bpool"
+	"github.com/wandera/regatta/raft"
+	"github.com/wandera/regatta/storage/table/key"
+	"go.uber.org/zap"
+
+	"github.com/cockroachdb/pebble"
+	sm "github.com/lni/dragonboat/v3/statemachine"
+	rp "github.com/wandera/regatta/pebble"
+	"github.com/wandera/regatta/proto"
+	pb "google.golang.org/protobuf/proto"
+)
+
+var (
+	bufferPool = bpool.NewSizedBufferPool(256, 128)
+	indexPool  = bpool.NewByteSlicePool(258, 8)
+)
+
+const (
+	// maxBatchSize maximum size of inmemory batch before commit.
+	maxBatchSize = 16 * 1024 * 1024
+)
+
+func New(tableName, stateMachineDir string, walDirname string, fs vfs.FS) sm.CreateOnDiskStateMachineFunc {
+	if fs == nil {
+		fs = vfs.Default
+	}
+	return func(clusterID uint64, nodeID uint64) sm.IOnDiskStateMachine {
+		return &SM{
+			pebble:     nil,
+			tableName:  tableName,
+			clusterID:  clusterID,
+			nodeID:     nodeID,
+			dirname:    stateMachineDir,
+			walDirname: walDirname,
+			fs:         fs,
+			log:        zap.S().Named("table").Named(tableName),
+		}
+	}
+}
+
+// SM is a statemachine.IOnDiskStateMachine impl.
+type SM struct {
+	pebble     unsafe.Pointer
+	wo         *pebble.WriteOptions
+	fs         vfs.FS
+	clusterID  uint64
+	nodeID     uint64
+	tableName  string
+	dirname    string
+	walDirname string
+	closed     bool
+	log        *zap.SugaredLogger
+}
+
+func (p *SM) Open(_ <-chan struct{}) (uint64, error) {
+	if p.clusterID < 1 {
+		return 0, raft.ErrInvalidClusterID
+	}
+	if p.nodeID < 1 {
+		return 0, raft.ErrInvalidNodeID
+	}
+	hostname, err := os.Hostname()
+	if err != nil {
+		return 0, err
+	}
+
+	dir := rp.GetNodeDBDirName(p.dirname, hostname, p.tableName)
+	if err := rp.CreateNodeDataDir(p.fs, dir); err != nil {
+		return 0, err
+	}
+
+	randomDir := rp.GetNewRandomDBDirName()
+	var dbdir string
+	if rp.IsNewRun(p.fs, dir) {
+		dbdir = filepath.Join(dir, randomDir)
+		if err := rp.SaveCurrentDBDirName(p.fs, dir, randomDir); err != nil {
+			return 0, err
+		}
+		if err := rp.ReplaceCurrentDBFile(p.fs, dir); err != nil {
+			return 0, err
+		}
+	} else {
+		if err := rp.CleanupNodeDataDir(p.fs, dir); err != nil {
+			return 0, err
+		}
+		var err error
+		randomDir, err = rp.GetCurrentDBDirName(p.fs, dir)
+		if err != nil {
+			return 0, err
+		}
+		dbdir = filepath.Join(dir, randomDir)
+		if _, err := p.fs.Stat(filepath.Join(dir, randomDir)); err != nil {
+			return 0, err
+		}
+	}
+
+	walDirPath := p.getWalDirPath(hostname, randomDir, dbdir)
+
+	p.log.Infof("opening pebble state machine with dirname: '%s', walDirName: '%s'", dbdir, walDirPath)
+	db, err := rp.OpenDB(p.fs, dbdir, walDirPath)
+	if err != nil {
+		return 0, err
+	}
+	atomic.StorePointer(&p.pebble, unsafe.Pointer(db))
+	p.wo = &pebble.WriteOptions{Sync: false}
+
+	buf := bytes.NewBuffer(make([]byte, 0))
+	k := key.Key{
+		KeyType: key.TypeSystem,
+		Key:     []byte{0x1},
+	}
+	if _, err := key.NewEncoder(buf).Encode(&k); err != nil {
+		return 0, err
+	}
+	indexVal, closer, err := db.Get(buf.Bytes())
+	if err != nil {
+		if err != pebble.ErrNotFound {
+			return 0, err
+		}
+		return 0, nil
+	}
+
+	defer func() {
+		if err := closer.Close(); err != nil {
+			p.log.Warn(err)
+		}
+	}()
+
+	return binary.LittleEndian.Uint64(indexVal), nil
+}
+
+func (p *SM) getWalDirPath(hostname string, randomDir string, dbdir string) string {
+	var walDirPath string
+	if p.walDirname != "" {
+		walDirPath = path.Join(p.walDirname, hostname, fmt.Sprintf("%d-%d", p.nodeID, p.clusterID), randomDir)
+	} else {
+		walDirPath = dbdir
+	}
+	return walDirPath
+}
+
+// Update updates the object.
+func (p *SM) Update(updates []sm.Entry) ([]sm.Entry, error) {
+	cmd := proto.Command{}
+	db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+	buf := bytes.NewBuffer(make([]byte, key.V1KeyLen))
+	enc := key.NewEncoder(buf)
+
+	batch := db.NewBatch()
+	defer batch.Close()
+
+	for i := 0; i < len(updates); i++ {
+		err := pb.Unmarshal(updates[i].Cmd, &cmd)
+		if err != nil {
+			return nil, err
+		}
+		buf.Reset()
+		k := key.Key{
+			KeyType: key.TypeUser,
+			Key:     cmd.Kv.Key,
+		}
+		if _, err := enc.Encode(&k); err != nil {
+			return nil, err
+		}
+
+		switch cmd.Type {
+		case proto.Command_PUT:
+			if err := batch.Set(buf.Bytes(), cmd.Kv.Value, nil); err != nil {
+				return nil, err
+			}
+		case proto.Command_DELETE:
+			if err := batch.Delete(buf.Bytes(), nil); err != nil {
+				return nil, err
+			}
+		}
+
+		updates[i].Result = sm.Result{Value: 1}
+	}
+
+	k := key.Key{
+		KeyType: key.TypeSystem,
+		Key:     []byte{0x1},
+	}
+
+	buf.Reset()
+	if _, err := enc.Encode(&k); err != nil {
+		return nil, err
+	}
+
+	idxBuffer := indexPool.GetSlice()
+	defer indexPool.PutSlice(idxBuffer)
+
+	binary.LittleEndian.PutUint64(idxBuffer.Bytes(), updates[len(updates)-1].Index)
+	if err := batch.Set(buf.Bytes(), idxBuffer.Bytes(), nil); err != nil {
+		return nil, err
+	}
+
+	if err := batch.Commit(p.wo); err != nil {
+		return nil, err
+	}
+	return updates, nil
+}
+
+// Lookup locally looks up the data.
+func (p *SM) Lookup(l interface{}) (interface{}, error) {
+	switch req := l.(type) {
+	case *proto.RangeRequest:
+		buf := bufferPool.Get()
+		defer bufferPool.Put(buf)
+		enc := key.NewEncoder(buf)
+		k := key.Key{
+			KeyType: key.TypeUser,
+			Key:     req.Key,
+		}
+		if _, err := enc.Encode(&k); err != nil {
+			return nil, err
+		}
+		db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+		value, closer, err := db.Get(buf.Bytes())
+		if err != nil {
+			return nil, err
+		}
+
+		defer func() {
+			if err := closer.Close(); err != nil {
+				p.log.Error(err)
+			}
+		}()
+
+		tmp := make([]byte, len(value))
+		copy(tmp, value)
+
+		return &proto.RangeResponse{
+			Kvs: []*proto.KeyValue{
+				{
+					Key:   req.Key,
+					Value: tmp,
+				},
+			},
+			Count: 1,
+		}, nil
+	case *proto.HashRequest:
+		hash, err := p.GetHash()
+		if err != nil {
+			return nil, err
+		}
+		return &proto.HashResponse{Hash: hash}, nil
+	}
+
+	return nil, raft.ErrUnknownQueryType
+}
+
+// Sync synchronizes all in-core state of the state machine to permanent
+// storage so the state machine can continue from its latest state after
+// reboot.
+func (p *SM) Sync() error {
+	db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+	return db.Flush()
+}
+
+// PrepareSnapshot prepares the snapshot to be concurrently captured and
+// streamed.
+func (p *SM) PrepareSnapshot() (interface{}, error) {
+	db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+	return db.NewSnapshot(), nil
+}
+
+// SaveSnapshot saves the state of the object to the provided io.Writer object.
+func (p *SM) SaveSnapshot(ctx interface{}, w io.Writer, _ <-chan struct{}) error {
+	snapshot := ctx.(*pebble.Snapshot)
+	iter := snapshot.NewIter(nil)
+	defer func() {
+		if err := iter.Close(); err != nil {
+			p.log.Error(err)
+		}
+		if err := snapshot.Close(); err != nil {
+			p.log.Error(err)
+		}
+	}()
+
+	// calculate the total snapshot size and send to writer
+	count := uint64(0)
+	for iter.First(); iter.Valid(); iter.Next() {
+		count++
+	}
+	err := binary.Write(w, binary.LittleEndian, count)
+	if err != nil {
+		return err
+	}
+
+	// iterate through he whole kv space and send it to writer
+	for iter.First(); iter.Valid(); iter.Next() {
+		kv := &proto.KeyValue{
+			Key:   iter.Key(),
+			Value: iter.Value(),
+		}
+		entry, err := pb.Marshal(kv)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(w, binary.LittleEndian, uint64(pb.Size(kv)))
+		if err != nil {
+			return err
+		}
+		if _, err := w.Write(entry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RecoverFromSnapshot recovers the state machine state from snapshot specified by
+// the io.Reader object. The snapshot is recovered into a new DB first and then
+// atomically swapped with the existing DB to complete the recovery.
+func (p *SM) RecoverFromSnapshot(r io.Reader, stopc <-chan struct{}) (er error) {
+	if p.closed {
+		return raft.ErrStateMachineClosed
+	}
+	if p.clusterID < 1 {
+		return raft.ErrInvalidClusterID
+	}
+	if p.nodeID < 1 {
+		return raft.ErrInvalidNodeID
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return err
+	}
+	dir := rp.GetNodeDBDirName(p.dirname, hostname, p.tableName)
+
+	randomDirName := rp.GetNewRandomDBDirName()
+	dbdir := filepath.Join(dir, randomDirName)
+	walDirPath := p.getWalDirPath(hostname, randomDirName, dbdir)
+
+	p.log.Infof("recovering pebble state machine with dirname: '%s', walDirName: '%s'", dbdir, walDirPath)
+	db, err := rp.OpenDB(p.fs, dbdir, walDirPath)
+	if err != nil {
+		return err
+	}
+
+	br := bufio.NewReaderSize(r, maxBatchSize)
+	var total uint64
+	if err := binary.Read(br, binary.LittleEndian, &total); err != nil {
+		return err
+	}
+
+	kv := proto.KeyValue{}
+	buffer := make([]byte, 0, 128*1024)
+	b := db.NewBatch()
+	defer b.Close()
+	p.log.Debugf("Starting snapshot recover to %s DB", dbdir)
+	var batchSize uint64
+	for i := uint64(0); i < total; i++ {
+		select {
+		case <-stopc:
+			_ = db.Close()
+			if err := rp.CleanupNodeDataDir(p.fs, dir); err != nil {
+				p.log.Debugf("unable to cleanup directory")
+			}
+			return sm.ErrSnapshotStopped
+		default:
+			p.log.Debugf("recover i %d", i)
+			var toRead uint64
+			if err := binary.Read(br, binary.LittleEndian, &toRead); err != nil {
+				return err
+			}
+
+			batchSize = batchSize + toRead
+			if cap(buffer) < int(toRead) {
+				buffer = make([]byte, toRead)
+			}
+
+			if _, err := io.ReadFull(br, buffer[:toRead]); err != nil {
+				return err
+			}
+			if err := pb.Unmarshal(buffer[:toRead], &kv); err != nil {
+				return err
+			}
+
+			if err := b.Set(kv.Key, kv.Value, nil); err != nil {
+				return err
+			}
+
+			if batchSize >= maxBatchSize {
+				err := b.Commit(nil)
+				if err != nil {
+					return err
+				}
+				b = db.NewBatch()
+				batchSize = 0
+			}
+		}
+	}
+
+	if err := b.Commit(&pebble.WriteOptions{Sync: false}); err != nil {
+		return err
+	}
+
+	if err := rp.SaveCurrentDBDirName(p.fs, dir, randomDirName); err != nil {
+		return err
+	}
+	if err := rp.ReplaceCurrentDBFile(p.fs, dir); err != nil {
+		return err
+	}
+	old := (*pebble.DB)(atomic.SwapPointer(&p.pebble, unsafe.Pointer(db)))
+	p.log.Debugf("Snapshot recovery finished")
+
+	if old != nil {
+		_ = old.Close()
+	}
+	p.log.Debugf("Snapshot recovery cleanup")
+	return rp.CleanupNodeDataDir(p.fs, dir)
+}
+
+// Close closes the KVStateMachine IStateMachine.
+func (p *SM) Close() error {
+	p.closed = true
+	db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+	if db == nil {
+		return nil
+	}
+	return db.Close()
+}
+
+// GetHash gets the DB hash for test comparison.
+func (p *SM) GetHash() (uint64, error) {
+	db := (*pebble.DB)(atomic.LoadPointer(&p.pebble))
+	snap := db.NewSnapshot()
+	iter := snap.NewIter(nil)
+	defer func() {
+		if err := iter.Close(); err != nil {
+			p.log.Error(err)
+		}
+	}()
+
+	// Compute Hash
+	hash64 := fnv.New64()
+	// iterate through he whole kv space and send it to hash func
+	for iter.First(); iter.Valid(); iter.Next() {
+		_, err := hash64.Write(iter.Key())
+		if err != nil {
+			return 0, err
+		}
+		_, err = hash64.Write(iter.Value())
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return hash64.Sum64(), nil
+}
---
+++ storage/table/sm_test.go
@@ -0,0 +1,554 @@
+package table
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/pebble/vfs"
+	sm "github.com/lni/dragonboat/v3/statemachine"
+	"github.com/stretchr/testify/require"
+	"github.com/wandera/regatta/proto"
+	"github.com/wandera/regatta/util"
+	"go.uber.org/zap"
+	pb "google.golang.org/protobuf/proto"
+)
+
+const (
+	testValue          = "test"
+	testTable          = "test"
+	testKeyFormat      = "test%d"
+	testLargeKeyFormat = "testlarge%d"
+)
+
+func TestSM_Open(t *testing.T) {
+	type fields struct {
+		clusterID  uint64
+		nodeID     uint64
+		dirname    string
+		walDirname string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "Invalid node ID",
+			fields: fields{
+				clusterID: 1,
+				nodeID:    0,
+				dirname:   "/tmp/dir",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid cluster ID",
+			fields: fields{
+				clusterID: 0,
+				nodeID:    1,
+				dirname:   "/tmp/dir",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Successfully open DB",
+			fields: fields{
+				clusterID: 1,
+				nodeID:    1,
+				dirname:   "/tmp/dir",
+			},
+		},
+		{
+			name: "Successfully open DB with WAL",
+			fields: fields{
+				clusterID:  1,
+				nodeID:     1,
+				dirname:    "/tmp/dir",
+				walDirname: "/tmp/waldir",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			p := &SM{
+				fs:         vfs.NewMem(),
+				clusterID:  tt.fields.clusterID,
+				nodeID:     tt.fields.nodeID,
+				dirname:    tt.fields.dirname,
+				walDirname: tt.fields.walDirname,
+				log:        zap.S(),
+			}
+			_, err := p.Open(nil)
+			if tt.wantErr {
+				r.Error(err)
+			} else {
+				r.NoError(err)
+				r.NoError(p.Close())
+			}
+		})
+	}
+}
+
+func emptySM() sm.IOnDiskStateMachine {
+	p := &SM{
+		fs:        vfs.NewMem(),
+		clusterID: 1,
+		nodeID:    1,
+		dirname:   "/tmp",
+		log:       zap.S(),
+	}
+	_, err := p.Open(nil)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	return p
+}
+
+func filledSM() sm.IOnDiskStateMachine {
+	const smallEntries = 10_000
+	const largeEntries = 10
+
+	entries := make([]sm.Entry, 0, smallEntries+largeEntries)
+	for i := 0; i < smallEntries; i++ {
+		entries = append(entries, sm.Entry{
+			Index: uint64(i),
+			Cmd: mustMarshallProto(&proto.Command{
+				Table: []byte(testTable),
+				Type:  proto.Command_PUT,
+				Kv: &proto.KeyValue{
+					Key:   []byte(fmt.Sprintf(testKeyFormat, i)),
+					Value: []byte(testValue),
+				},
+			}),
+		})
+	}
+	for i := 0; i < largeEntries; i++ {
+		entries = append(entries, sm.Entry{
+			Index: uint64(i),
+			Cmd: mustMarshallProto(&proto.Command{
+				Table: []byte(testTable),
+				Type:  proto.Command_PUT,
+				Kv: &proto.KeyValue{
+					Key:   []byte(fmt.Sprintf(testLargeKeyFormat, i)),
+					Value: []byte(largeValues[i]),
+				},
+			}),
+		})
+	}
+	p := &SM{
+		fs:        vfs.NewMem(),
+		clusterID: 1,
+		nodeID:    1,
+		dirname:   "/tmp",
+		log:       zap.S(),
+	}
+	_, err := p.Open(nil)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	_, err = p.Update(entries)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	return p
+}
+
+func filledLargeValuesSM() sm.IOnDiskStateMachine {
+	entries := make([]sm.Entry, len(largeValues))
+	for i := 0; i < len(entries); i++ {
+		entries[i] = sm.Entry{
+			Index: uint64(i),
+			Cmd: mustMarshallProto(&proto.Command{
+				Table: []byte(testTable),
+				Type:  proto.Command_PUT,
+				Kv: &proto.KeyValue{
+					Key:   []byte(fmt.Sprintf(testKeyFormat, i)),
+					Value: []byte(largeValues[i]),
+				},
+			}),
+		}
+	}
+	p := &SM{
+		fs:        vfs.NewMem(),
+		clusterID: 1,
+		nodeID:    1,
+		dirname:   "/tmp",
+		log:       zap.S(),
+	}
+	_, err := p.Open(nil)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	_, err = p.Update(entries)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	return p
+}
+
+var largeValues []string
+
+func init() {
+	for i := 0; i < 10_000; i++ {
+		largeValues = append(largeValues, util.RandString(2048))
+	}
+}
+
+func TestSM_Snapshot(t *testing.T) {
+	type args struct {
+		producingSMFactory func() sm.IOnDiskStateMachine
+		receivingSMFactory func() sm.IOnDiskStateMachine
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"Pebble -> Pebble",
+			args{
+				producingSMFactory: filledSM,
+				receivingSMFactory: emptySM,
+			},
+		},
+		{
+			"Pebble(large) -> Pebble",
+			args{
+				producingSMFactory: filledLargeValuesSM,
+				receivingSMFactory: emptySM,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Log("Applying snapshot to the empty DB should produce the same hash")
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			p := tt.args.producingSMFactory()
+			defer p.Close()
+
+			want, err := p.(sm.IHash).GetHash()
+			r.NoError(err)
+
+			snp, err := p.PrepareSnapshot()
+			r.NoError(err)
+
+			ep := tt.args.receivingSMFactory()
+			defer ep.Close()
+
+			snapf, err := os.Create(filepath.Join(t.TempDir(), "snapshot-file"))
+			if err == nil {
+				defer snapf.Close()
+			}
+			r.NoError(err)
+
+			t.Log("Save snapshot started")
+			err = p.SaveSnapshot(snp, snapf, nil)
+			r.NoError(err)
+			_, err = snapf.Seek(0, 0)
+			r.NoError(err)
+
+			t.Log("Recover from snapshot started")
+			stopc := make(chan struct{})
+			err = ep.RecoverFromSnapshot(snapf, stopc)
+			r.NoError(err)
+
+			t.Log("Recovery finished")
+
+			got, err := ep.(sm.IHash).GetHash()
+			r.NoError(err)
+			r.Equal(want, got, "the hash of recovered DB should be the same as of the original one")
+		})
+	}
+}
+
+func TestSM_Snapshot_Stopped(t *testing.T) {
+	type args struct {
+		producingSMFactory func() sm.IOnDiskStateMachine
+		receivingSMFactory func() sm.IOnDiskStateMachine
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"Pebble(large) -> Pebble",
+			args{
+				producingSMFactory: filledLargeValuesSM,
+				receivingSMFactory: emptySM,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Log("Applying snapshot to the empty DB should be stopped")
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			p := tt.args.producingSMFactory()
+			defer p.Close()
+
+			snp, err := p.PrepareSnapshot()
+			r.NoError(err)
+
+			ep := tt.args.receivingSMFactory()
+
+			snapf, err := os.Create(filepath.Join(t.TempDir(), "snapshot-file"))
+			r.NoError(err)
+			t.Log("Save snapshot started")
+			err = p.SaveSnapshot(snp, snapf, nil)
+			r.NoError(err)
+			_, err = snapf.Seek(0, 0)
+			r.NoError(err)
+
+			stopc := make(chan struct{})
+			go func() {
+				defer func() {
+					_ = snapf.Close()
+					_ = ep.Close()
+					_ = snapf.Close()
+				}()
+				t.Log("Recover from snapshot routine started")
+				err := ep.RecoverFromSnapshot(snapf, stopc)
+				r.Error(err)
+				r.Equal(sm.ErrSnapshotStopped, err)
+			}()
+
+			time.Sleep(10 * time.Millisecond)
+			close(stopc)
+
+			t.Log("Recovery stopped")
+		})
+	}
+}
+
+func TestSM_Lookup(t *testing.T) {
+	type fields struct {
+		smFactory func() sm.IOnDiskStateMachine
+	}
+	type args struct {
+		key *proto.RangeRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "Pebble - Lookup empty DB",
+			fields: fields{
+				smFactory: emptySM,
+			},
+			args: args{key: &proto.RangeRequest{
+				Key: []byte("Hello"),
+			}},
+			wantErr: true,
+		},
+		{
+			name: "Pebble - Lookup full DB with non-existent key",
+			fields: fields{
+				smFactory: filledSM,
+			},
+			args: args{key: &proto.RangeRequest{
+				Key: []byte("Hello"),
+			}},
+			wantErr: true,
+		},
+		{
+			name: "Pebble - Lookup full DB with existing key",
+			fields: fields{
+				smFactory: filledSM,
+			},
+			args: args{key: &proto.RangeRequest{
+				Table: []byte(testTable),
+				Key:   []byte(fmt.Sprintf(testKeyFormat, 0)),
+			}},
+			want: &proto.RangeResponse{
+				Kvs: []*proto.KeyValue{
+					{
+						Key:   []byte(fmt.Sprintf(testKeyFormat, 0)),
+						Value: []byte(testValue),
+					},
+				},
+				Count: 1,
+			},
+		},
+		{
+			name: "Pebble - Lookup full DB with existing key and large value",
+			fields: fields{
+				smFactory: filledSM,
+			},
+			args: args{key: &proto.RangeRequest{
+				Table: []byte(testTable),
+				Key:   []byte(fmt.Sprintf(testLargeKeyFormat, 0)),
+			}},
+			want: &proto.RangeResponse{
+				Kvs: []*proto.KeyValue{
+					{
+						Key:   []byte(fmt.Sprintf(testLargeKeyFormat, 0)),
+						Value: []byte(largeValues[0]),
+					},
+				},
+				Count: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			p := tt.fields.smFactory()
+			defer func() {
+				r.NoError(p.Close())
+			}()
+			got, err := p.Lookup(tt.args.key)
+			if tt.wantErr {
+				r.Error(err)
+				return
+			}
+			r.NoError(err)
+			r.Equal(tt.want, got)
+		})
+	}
+}
+
+func TestSM_Update(t *testing.T) {
+	type fields struct {
+		smFactory func() sm.IOnDiskStateMachine
+	}
+	type args struct {
+		updates []sm.Entry
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []sm.Entry
+		wantErr bool
+	}{
+		{
+			name: "Pebble - Successful update of a single item",
+			fields: fields{
+				smFactory: emptySM,
+			},
+			args: args{
+				updates: []sm.Entry{
+					{
+						Index: 1,
+						Cmd: mustMarshallProto(&proto.Command{
+							Table: []byte("test"),
+							Type:  proto.Command_PUT,
+							Kv: &proto.KeyValue{
+								Key:   []byte("test"),
+								Value: []byte("test"),
+							},
+						}),
+					},
+				},
+			},
+			want: []sm.Entry{
+				{
+					Index: 1,
+					Cmd: mustMarshallProto(&proto.Command{
+						Table: []byte("test"),
+						Type:  proto.Command_PUT,
+						Kv: &proto.KeyValue{
+							Key:   []byte("test"),
+							Value: []byte("test"),
+						},
+					}),
+					Result: sm.Result{
+						Value: 1,
+						Data:  nil,
+					},
+				},
+			},
+		},
+		{
+			name: "Pebble - Successful update of a batch",
+			fields: fields{
+				smFactory: emptySM,
+			},
+			args: args{
+				updates: []sm.Entry{
+					{
+						Index: 1,
+						Cmd: mustMarshallProto(&proto.Command{
+							Table: []byte("test"),
+							Type:  proto.Command_PUT,
+							Kv: &proto.KeyValue{
+								Key:   []byte("test"),
+								Value: []byte("test"),
+							},
+						}),
+					},
+					{
+						Index: 2,
+						Cmd: mustMarshallProto(&proto.Command{
+							Table: []byte("test"),
+							Type:  proto.Command_DELETE,
+							Kv: &proto.KeyValue{
+								Key: []byte("test"),
+							},
+						}),
+					},
+				},
+			},
+			want: []sm.Entry{
+				{
+					Index: 1,
+					Cmd: mustMarshallProto(&proto.Command{
+						Table: []byte("test"),
+						Type:  proto.Command_PUT,
+						Kv: &proto.KeyValue{
+							Key:   []byte("test"),
+							Value: []byte("test"),
+						},
+					}),
+					Result: sm.Result{
+						Value: 1,
+						Data:  nil,
+					},
+				},
+				{
+					Index: 2,
+					Cmd: mustMarshallProto(&proto.Command{
+						Table: []byte("test"),
+						Type:  proto.Command_DELETE,
+						Kv: &proto.KeyValue{
+							Key: []byte("test"),
+						},
+					}),
+					Result: sm.Result{
+						Value: 1,
+						Data:  nil,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := require.New(t)
+			p := tt.fields.smFactory()
+			defer func() {
+				r.NoError(p.Close())
+			}()
+			got, err := p.Update(tt.args.updates)
+			if tt.wantErr {
+				r.Error(err)
+				return
+			}
+			r.Equal(tt.want, got)
+		})
+	}
+}
+
+func mustMarshallProto(message pb.Message) []byte {
+	bytes, err := pb.Marshal(message)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+	return bytes
+}
