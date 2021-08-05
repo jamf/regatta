7fe7f93b084c89b63e43ca5a0582b0d20f736f16
jakub.coufal <jakub.coufal@wandera.com>
2021-08-05 12:41:00 +0200

    WND-37956 - Refactor tests and add log replication test

--- replication/metadata_test.go
+++ replication/metadata_test.go
@@ -1,56 +1,33 @@
 package replication
 
 import (
-	"fmt"
-	"net"
 	"testing"
 	"time"
 
 	pvfs "github.com/cockroachdb/pebble/vfs"
-	"github.com/lni/dragonboat/v3"
-	"github.com/lni/dragonboat/v3/config"
-	"github.com/lni/vfs"
 	"github.com/stretchr/testify/require"
 	"github.com/wandera/regatta/proto"
-	"github.com/wandera/regatta/regattaserver"
 	"github.com/wandera/regatta/storage/tables"
 	"google.golang.org/grpc"
 )
 
-var tableManagerTestConfig = tables.Config{
-	NodeID: 1,
-	Table:  tables.TableConfig{HeartbeatRTT: 1, ElectionRTT: 5, FS: pvfs.NewMem()},
-	Meta:   tables.MetaConfig{HeartbeatRTT: 1, ElectionRTT: 5},
+var tableManagerTestConfig = func() tables.Config {
+	return tables.Config{
+		NodeID: 1,
+		Table:  tables.TableConfig{HeartbeatRTT: 1, ElectionRTT: 5, FS: pvfs.NewMem()},
+		Meta:   tables.MetaConfig{HeartbeatRTT: 1, ElectionRTT: 5},
+	}
 }
 
 func TestMetadata_Replicate(t *testing.T) {
 	r := require.New(t)
-	t.Log("start leader Raft")
-	leaderNH, leaderAddresses, err := startRaftNode()
-	r.NoError(err)
-	defer leaderNH.Close()
-
-	t.Log("create leader table manager")
-	leaderTM := tables.NewManager(leaderNH, leaderAddresses, tableManagerTestConfig)
-	r.NoError(leaderTM.Start())
-	r.NoError(leaderTM.WaitUntilReady())
-	defer leaderTM.Close()
-	t.Log("start the metadata server")
-	metaSrv := startMetadataServer(leaderTM)
-	defer metaSrv.Shutdown()
-
-	t.Log("start follower Raft")
-	followerNH, followerAddresses, err := startRaftNode()
-	r.NoError(err)
-
-	t.Log("create follower table manager")
-	followerTM := tables.NewManager(followerNH, followerAddresses, tableManagerTestConfig)
-	r.NoError(followerTM.Start())
-	r.NoError(followerTM.WaitUntilReady())
-	defer followerTM.Close()
+	leaderTM, followerTM, leaderNH, _, closer := prepareLeaderAndFollowerRaft(t)
+	defer closer()
+	srv := startReplicationServer(leaderTM, leaderNH)
+	defer srv.Shutdown()
 
 	t.Log("create replicator")
-	conn, err := grpc.Dial(metaSrv.Addr, grpc.WithInsecure())
+	conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
 	r.NoError(err)
 	metar := NewMetadata(proto.NewMetadataClient(conn), followerTM)
 	metar.Interval = 250 * time.Millisecond
@@ -80,42 +57,4 @@ func TestMetadata_Replicate(t *testing.T) {
 	tabs, err := followerTM.GetTables()
 	r.NoError(err)
 	r.Len(tabs, 2)
-}
-
-func startMetadataServer(manager *tables.Manager) *regattaserver.RegattaServer {
-	testNodeAddress := fmt.Sprintf("localhost:%d", getTestPort())
-	server := regattaserver.NewServer(testNodeAddress, false)
-	proto.RegisterMetadataServer(server, &regattaserver.MetadataServer{Tables: manager})
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			panic(err)
-		}
-	}()
-	return server
-}
-
-func startRaftNode() (*dragonboat.NodeHost, map[uint64]string, error) {
-	testNodeAddress := fmt.Sprintf("localhost:%d", getTestPort())
-	nhc := config.NodeHostConfig{
-		WALDir:         "wal",
-		NodeHostDir:    "dragonboat",
-		RTTMillisecond: 1,
-		RaftAddress:    testNodeAddress,
-	}
-	_ = nhc.Prepare()
-	nhc.Expert.FS = vfs.NewMem()
-	nhc.Expert.Engine.ExecShards = 1
-	nhc.Expert.LogDB.Shards = 1
-	nh, err := dragonboat.NewNodeHost(nhc)
-	if err != nil {
-		return nil, nil, err
-	}
-	return nh, map[uint64]string{1: testNodeAddress}, nil
-}
-
-func getTestPort() int {
-	l, _ := net.Listen("tcp", ":0")
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port
 }
--- replication/worker_test.go
+++ replication/worker_test.go
@@ -2,41 +2,71 @@ package replication
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/lni/dragonboat/v3"
 	"github.com/stretchr/testify/require"
 	"github.com/wandera/regatta/proto"
+	"github.com/wandera/regatta/regattaserver"
 	"github.com/wandera/regatta/storage/tables"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 	"google.golang.org/grpc"
 )
 
+func Test_worker_do(t *testing.T) {
+	r := require.New(t)
+	leaderTM, followerTM, leaderNH, followerNH, closer := prepareLeaderAndFollowerRaft(t)
+	defer closer()
+	srv := startReplicationServer(leaderTM, leaderNH)
+	defer srv.Shutdown()
+
+	t.Log("create tables")
+	r.NoError(leaderTM.CreateTable("test"))
+	r.NoError(followerTM.CreateTable("test"))
+	time.Sleep(5 * time.Second)
+
+	t.Log("load some data")
+	at, err := leaderTM.GetTable("test")
+	r.NoError(err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	for i := 0; i < 50; i++ {
+		_, err = at.Put(ctx, &proto.PutRequest{
+			Key:   []byte(fmt.Sprintf("foo-%d", i)),
+			Value: []byte("bar"),
+		})
+		r.NoError(err)
+	}
+
+	t.Log("create worker")
+	conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
+	r.NoError(err)
+	w := &worker{Table: "test", logTimeout: time.Minute, tm: followerTM, logClient: proto.NewLogClient(conn), nh: followerNH, log: zaptest.NewLogger(t).Sugar()}
+	r.NoError(w.do())
+	table, err := followerTM.GetTable("test")
+	r.NoError(err)
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	response, err := table.Range(ctx, &proto.RangeRequest{
+		Table:        []byte("test"),
+		Key:          []byte{0},
+		RangeEnd:     []byte{0},
+		Linearizable: true,
+		CountOnly:    true,
+	})
+	r.NoError(err)
+	r.Equal(int64(50), response.Count)
+}
+
 func Test_worker_recover(t *testing.T) {
 	r := require.New(t)
-	t.Log("start leader Raft")
-	leaderNH, leaderAddresses, err := startRaftNode()
-	r.NoError(err)
-	defer leaderNH.Close()
-
-	t.Log("create leader table manager")
-	leaderTM := tables.NewManager(leaderNH, leaderAddresses, tableManagerTestConfig)
-	r.NoError(leaderTM.Start())
-	r.NoError(leaderTM.WaitUntilReady())
-	defer leaderTM.Close()
-	t.Log("start the metadata server")
-	metaSrv := startSnapshotServer(leaderTM)
-	defer metaSrv.Shutdown()
-
-	t.Log("start follower Raft")
-	followerNH, followerAddresses, err := startRaftNode()
-	r.NoError(err)
-
-	t.Log("create follower table manager")
-	followerTM := tables.NewManager(followerNH, followerAddresses, tableManagerTestConfig)
-	r.NoError(followerTM.Start())
-	r.NoError(followerTM.WaitUntilReady())
-	defer followerTM.Close()
+	leaderTM, followerTM, leaderNH, _, closer := prepareLeaderAndFollowerRaft(t)
+	defer closer()
+	srv := startReplicationServer(leaderTM, leaderNH)
+	defer srv.Shutdown()
 
 	t.Log("create tables")
 	r.NoError(leaderTM.CreateTable("test"))
@@ -55,7 +85,7 @@ func Test_worker_recover(t *testing.T) {
 	r.NoError(err)
 
 	t.Log("create worker")
-	conn, err := grpc.Dial(metaSrv.Addr, grpc.WithInsecure())
+	conn, err := grpc.Dial(srv.Addr, grpc.WithInsecure())
 	r.NoError(err)
 	w := &worker{Table: "test", snapshotTimeout: time.Minute, tm: followerTM, snapshotClient: proto.NewSnapshotClient(conn), log: zaptest.NewLogger(t).Sugar()}
 
@@ -71,4 +101,52 @@ func Test_worker_recover(t *testing.T) {
 	tab, err = followerTM.GetTable("test2")
 	r.NoError(err)
 	r.Equal("test2", tab.Name)
+}
+
+func prepareLeaderAndFollowerRaft(t *testing.T) (leaderTM *tables.Manager, followerTM *tables.Manager, leaderNH *dragonboat.NodeHost, followerNH *dragonboat.NodeHost, closer func()) {
+	r := require.New(t)
+	t.Log("start leader Raft")
+	leaderNH, leaderAddresses, err := startRaftNode()
+	r.NoError(err)
+
+	t.Log("create leader table manager")
+	leaderTM = tables.NewManager(leaderNH, leaderAddresses, tableManagerTestConfig())
+	r.NoError(leaderTM.Start())
+	r.NoError(leaderTM.WaitUntilReady())
+
+	t.Log("start follower Raft")
+	followerNH, followerAddresses, err := startRaftNode()
+	r.NoError(err)
+
+	t.Log("create follower table manager")
+	followerTM = tables.NewManager(followerNH, followerAddresses, tableManagerTestConfig())
+	r.NoError(followerTM.Start())
+	r.NoError(followerTM.WaitUntilReady())
+
+	closer = func() {
+		leaderTM.Close()
+		leaderNH.Close()
+		followerTM.Close()
+		followerNH.Close()
+	}
+	return
+}
+
+func startReplicationServer(manager *tables.Manager, nh *dragonboat.NodeHost) *regattaserver.RegattaServer {
+	testNodeAddress := fmt.Sprintf("localhost:%d", getTestPort())
+	server := regattaserver.NewServer(testNodeAddress, false)
+	db, err := nh.GetReadOnlyLogDB()
+	if err != nil {
+		panic(err)
+	}
+	proto.RegisterMetadataServer(server, &regattaserver.MetadataServer{Tables: manager})
+	proto.RegisterSnapshotServer(server, &regattaserver.SnapshotServer{Tables: manager})
+	proto.RegisterLogServer(server, &regattaserver.LogServer{Tables: manager, NodeID: manager.NodeID(), Log: zap.NewNop().Sugar(), DB: db})
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	return server
 }
