f4c1f7da54a917aa2b8b1566f5fd2df29534e4f4
jakub.coufal <jakub.coufal@wandera.com>
2021-08-11 16:31:16 +0200

    WND-37956 - Limit max number of recoveries running per node

--- replication/replication.go
+++ replication/replication.go
@@ -37,15 +37,16 @@ func NewManager(tm *tables.Manager, nh *dragonboat.NodeHost, conn *grpc.ClientCo
 		Interval: 30 * time.Second,
 		tm:       tm,
 		factory: &workerFactory{
-			interval:        10 * time.Second,
-			leaseInterval:   10 * time.Second,
-			logTimeout:      5 * time.Minute,
-			snapshotTimeout: 1 * time.Hour,
-			tm:              tm,
-			log:             replicationLog,
-			nh:              nh,
-			logClient:       proto.NewLogClient(conn),
-			snapshotClient:  proto.NewSnapshotClient(conn),
+			interval:            10 * time.Second,
+			leaseInterval:       10 * time.Second,
+			logTimeout:          5 * time.Minute,
+			snapshotTimeout:     1 * time.Hour,
+			maxParallelRecovery: 2,
+			tm:                  tm,
+			log:                 replicationLog,
+			nh:                  nh,
+			logClient:           proto.NewLogClient(conn),
+			snapshotClient:      proto.NewSnapshotClient(conn),
 			metrics: struct {
 				replicationIndex  *prometheus.GaugeVec
 				replicationLeased *prometheus.GaugeVec
