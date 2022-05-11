acea5868f8a872d4e491a7a5b577ca3e98f37590
jakub.coufal <jakub.coufal@wandera.com>
2022-05-11 08:45:30 +0200

    TRIVIAL - Bump to Go 1.18, remove obsolete tools

--- tools.go
+++ tools.go
@@ -4,9 +4,6 @@
 package tools
 
 import (
-	_ "github.com/AlekSi/gocov-xml"
-	_ "github.com/axw/gocov/gocov"
-	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto"
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
