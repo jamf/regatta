44173c7739f5c6e3fc27a51e5032dfca57c3ca7c
jakub.coufal <jakub.coufal@wandera.com>
2022-09-04 12:00:42 +0200

    Send SSTs over wire when requesting local snapshot

---
+++ storage/table/fsm/encode.go
@@ -0,0 +1,49 @@
+package fsm
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+)
+
+// memFile is a file-like struct that buffers all data written to it in memory.
+// Implements the writeCloseSyncer interface.
+type memFile struct {
+	bytes.Buffer
+}
+
+// Close implements the writeCloseSyncer interface.
+func (*memFile) Close() error {
+	return nil
+}
+
+// Sync implements the writeCloseSyncer interface.
+func (*memFile) Sync() error {
+	return nil
+}
+
+// Data returns the in-memory buffer behind this MemFile.
+func (f *memFile) Data() []byte {
+	return f.Bytes()
+}
+
+// Flush is implemented so it prevents buffering inside Writter.
+func (f *memFile) Flush() error {
+	return nil
+}
+
+type lenReader interface {
+	io.Reader
+	Len() int
+}
+
+func writeLenDelimited(from lenReader, to io.Writer) error {
+	err := binary.Write(to, binary.LittleEndian, uint64(from.Len()))
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(to, from); err != nil {
+		return err
+	}
+	return nil
+}
