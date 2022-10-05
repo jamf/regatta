5fc177efa99ef6aa4f777ba27533ce1230b56e50
jakub.coufal <jakub.coufal@wandera.com>
2022-10-05 09:56:45 +0200

    Move update back to the original struct

--- storage/table/fsm/fsm.go
+++ storage/table/fsm/fsm.go
@@ -16,6 +16,7 @@ import (
 	"github.com/oxtoacart/bpool"
 	"github.com/prometheus/client_golang/prometheus"
 	rp "github.com/wandera/regatta/pebble"
+	"github.com/wandera/regatta/proto"
 	"github.com/wandera/regatta/storage/errors"
 	"github.com/wandera/regatta/storage/table/key"
 	"go.uber.org/zap"
@@ -156,6 +157,64 @@ func (p *FSM) Open(_ <-chan struct{}) (uint64, error) {
 	}
 
 	return readLocalIndex(db, sysLocalIndex)
+}
+
+// Update advances the FSM.
+func (p *FSM) Update(updates []sm.Entry) ([]sm.Entry, error) {
+	db := p.pebble.Load()
+
+	ctx := &updateContext{
+		batch: db.NewBatch(),
+		db:    db,
+		wo:    p.wo,
+		cmd:   proto.CommandFromVTPool(),
+	}
+
+	defer func() {
+		_ = ctx.Close()
+	}()
+
+	for i := 0; i < len(updates); i++ {
+		if err := ctx.Init(updates[i]); err != nil {
+			return nil, err
+		}
+
+		var cmd command
+		switch ctx.cmd.Type {
+		case proto.Command_PUT:
+			cmd = commandPut{ctx}
+		case proto.Command_DELETE:
+			cmd = commandDelete{ctx}
+		case proto.Command_PUT_BATCH:
+			cmd = commandPutBatch{ctx}
+		case proto.Command_DELETE_BATCH:
+			cmd = commandDeleteBatch{ctx}
+		case proto.Command_TXN:
+			cmd = commandTxn{ctx}
+		case proto.Command_DUMMY:
+			cmd = commandDummy{ctx}
+		}
+
+		updateResult, res, err := cmd.handle()
+		if err != nil {
+			return nil, err
+		}
+
+		if len(res.Responses) > 0 {
+			bts, err := res.MarshalVT()
+			if err != nil {
+				return nil, err
+			}
+			updates[i].Result.Data = bts
+		}
+		updates[i].Result.Value = uint64(updateResult)
+	}
+
+	if err := ctx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return updates, nil
 }
 
 // Sync synchronizes all in-core state of the state machine to permanent
