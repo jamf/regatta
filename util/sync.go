4a09b2630b5102a4de100b873f3b84b2c6b98b6e
jakub.coufal <jakub.coufal@wandera.com>
2022-10-19 12:06:50 +0200

    Fix nil-safety for SyncMap

--- util/sync.go
+++ util/sync.go
@@ -5,43 +5,45 @@ import (
 )
 
 type SyncMap[K comparable, V any] struct {
-	m sync.Map
+	m   map[K]V
+	mtx sync.RWMutex
 }
 
 func (s *SyncMap[K, V]) Load(key K) (V, bool) {
-	v, ok := s.m.Load(key)
-	if ok {
-		return v.(V), ok
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	if s.m == nil {
+		return *new(V), false
 	}
-	return *new(V), ok
+	v, ok := s.m[key]
+	return v, ok
 }
 
 func (s *SyncMap[K, V]) Store(key K, val V) {
-	s.m.Store(key, val)
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	if s.m == nil {
+		s.m = make(map[K]V)
+	}
+	s.m[key] = val
 }
 
-func (s *SyncMap[K, V]) LoadOrStore(key K, val V) (V, bool) {
-	v, ok := s.m.LoadOrStore(key, val)
+func (s *SyncMap[K, V]) ComputeIfAbsent(key K, valFunc func(K) V) V {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	if s.m == nil {
+		s.m = make(map[K]V)
+	}
+	v, ok := s.m[key]
 	if !ok {
-		return v.(V), ok
+		v = valFunc(key)
+		s.m[key] = v
 	}
-	return *new(V), ok
-}
-
-func (s *SyncMap[K, V]) LoadAndDelete(key K) (V, bool) {
-	v, ok := s.m.LoadAndDelete(key)
-	if ok {
-		return v.(V), ok
-	}
-	return *new(V), ok
+	return v
 }
 
 func (s *SyncMap[K, V]) Delete(key K) {
-	s.m.Delete(key)
-}
-
-func (s *SyncMap[K, V]) Range(f func(K, V) bool) {
-	s.m.Range(func(key, value any) bool {
-		return f(key.(K), value.(V))
-	})
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	delete(s.m, key)
 }
